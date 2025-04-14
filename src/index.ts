1eb0dd54ea1659efa77907f59ca289d9a2af9562
Michele Riva <ciao@micheleriva.it>
2025-04-14 11:09:29 +0200

    exports cloud methods

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/types.ts'
 export * from './manager.ts'
 export * from './collection.ts'
+export * from './cloud.ts'
 export type { AnswerConfig, AnswerSessionConfig, Interaction, PlanExecution } from './answer-session.ts'
 
 export { AnswerSession } from './answer-session.ts'
