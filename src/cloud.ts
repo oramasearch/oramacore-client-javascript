9b03051e02d9a9d29f91ea3588fcb1267d3126d3
Michele Riva <michele.riva@oramasearch.com>
2025-08-06 20:48:35 -0700

    refactors collection manager

--- src/cloud.ts
+++ src/cloud.ts
@@ -1,13 +1,5 @@
-import type {
-  AnyObject,
-  Maybe,
-  NLPSearchResult,
-  NLPSearchStreamResult,
-  SearchParams,
-  SearchResult,
-} from './lib/types.ts'
-import type { Index, NLPSearchParams } from './collection.ts'
-import type { CreateAISessionConfig, OramaCoreStream } from './stream-manager.ts'
+import type { AnyObject, SearchParams, SearchResult } from './lib/types.ts'
+import type { Index } from './collection.ts'
 
 import { CollectionManager } from './collection.ts'
 
@@ -26,16 +18,31 @@ export interface ProjectManagerConfig {
 export class OramaCloud {
   private client: CollectionManager
 
-  public identity: IdentityNamespace
-  public ai: AINamespace
+  // Expose all namespaces from CollectionManager
+  public identity: CollectionManager['identity']
+  public ai: CollectionManager['ai']
+  public collections: CollectionManager['collections']
+  public index: CollectionManager['index']
+  public hooks: CollectionManager['hooks']
+  public logs: CollectionManager['logs']
+  public systemPrompts: CollectionManager['systemPrompts']
+  public tools: CollectionManager['tools']
 
   constructor(config: ProjectManagerConfig) {
     this.client = new CollectionManager({
       ...config,
       collectionID: config.projectId,
     })
-    this.identity = new IdentityNamespace(this.client)
-    this.ai = new AINamespace(this.client)
+
+    // Delegate to CollectionManager namespaces
+    this.identity = this.client.identity
+    this.ai = this.client.ai
+    this.collections = this.client.collections
+    this.index = this.client.index
+    this.hooks = this.client.hooks
+    this.logs = this.client.logs
+    this.systemPrompts = this.client.systemPrompts
+    this.tools = this.client.tools
   }
 
   search(params: OramaCloudSearchParams): Promise<SearchResult> {
@@ -44,7 +51,7 @@ export class OramaCloud {
   }
 
   dataSource(id: string) {
-    const index = this.client.setIndex(id)
+    const index = this.client.index.set(id)
     return new DataSourceNamespace(index)
   }
 }
@@ -56,64 +63,19 @@ class DataSourceNamespace {
     this.index = index
   }
 
-  reindex(): Promise<Index> {
+  reindex(): Promise<void> {
     return this.index.reindex()
   }
-  insertDocuments(documents: AnyObject | AnyObject[]): Promise<Index> {
+
+  insertDocuments(documents: AnyObject | AnyObject[]): Promise<void> {
     return this.index.insertDocuments(documents)
   }
-  deleteDocuments(documentIDs: string | string[]): Promise<Index> {
+
+  deleteDocuments(documentIDs: string | string[]): Promise<void> {
     return this.index.deleteDocuments(documentIDs)
   }
-  upsertDocuments(documents: AnyObject[]): Promise<Index> {
+
+  upsertDocuments(documents: AnyObject[]): Promise<void> {
     return this.index.upsertDocuments(documents)
-  }
-}
-
-class IdentityNamespace {
-  private client: CollectionManager
-
-  constructor(client: CollectionManager) {
-    this.client = client
-  }
-
-  getIdentity(): Maybe<string> {
-    return this.client.getIdentity()
-  }
-
-  getUserId(): Maybe<string> {
-    return this.client.getUserId()
-  }
-
-  identify(userId: string): Promise<void> {
-    return this.client.identify(userId)
-  }
-
-  alias(alias: string): Promise<void> {
-    return this.client.alias(alias)
-  }
-
-  reset() {
-    return this.client.reset()
-  }
-}
-
-class AINamespace {
-  private client: CollectionManager
-
-  constructor(client: CollectionManager) {
-    this.client = client
-  }
-
-  NLPSearch(params: NLPSearchParams): Promise<NLPSearchResult<AnyObject>[]> {
-    return this.client.NLPSearch(params)
-  }
-
-  NLPSearchStream<R = AnyObject>(params: NLPSearchParams): AsyncGenerator<NLPSearchStreamResult<R>, void, unknown> {
-    return this.client.NLPSearchStream(params)
-  }
-
-  createAISession(config: CreateAISessionConfig): OramaCoreStream {
-    return this.client.createAISession(config)
   }
 }
