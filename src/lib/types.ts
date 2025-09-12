52bed24cde93bd076c9f5ef77a9ebe1133b1f431
niltonheck <heck.nilton@gmail.com>
2025-09-12 13:49:26 -0300

    fix: restore threshold property in SearchParams type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -73,6 +73,7 @@ export type SearchParams = {
   datasourceIDs?: string[]
   boost?: { [key: string]: number }
   exact?: boolean
+  threshold?: number
   similarity?: number
   tolerance?: number
   userID?: string
